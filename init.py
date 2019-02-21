4dea6fb10e2d99bf09fddf50a8475181ce369d41
kdy <laasl@naver.com>
2019-02-21 14:58:03 +0900

    init, meun update

--- init.py
+++ init.py
@@ -5,3 +5,8 @@ nuke.pluginAddPath("./gizmos", addToSysPath=True)
 nuke.pluginAddPath("./images", addToSysPath=True)
 nuke.pluginAddPath("./lib", addToSysPath=True)
 nuke.pluginAddPath("./scripts", addToSysPath=True)
+
+nukePath = os.environ["NUKE_PATH"]
+nuke.ViewerProcess.register("AlexazV3LogC", nuke.createNode, ("Vectorfield", "vfield_file %s/luts/ARRI_LogC2Video_Classic709_davinci3d.cube colorspaceIn AlexaV3LogC" % (nukePath)))
+nuke.addFormat("24 24 nukeIcon")
+nuke.addFormat("360 360 kakaoEmoticon")
--- menu.py
+++ menu.py
@@ -1,4 +1,8 @@
 import nuke
+import nukescripts
+import checkenv
+import openfile
+#import openfile_v01
 
 tb = nuke.toolbar("Nodes")
 m = tb.addMenu("kdy", icon="icon.png")
@@ -7,3 +11,16 @@ m.addCommand("Draw/timecode_burnin", "nuke.createNode('timecode_burnin')")
 m.addCommand("Draw/slate", "nuke.createNode('slate')")
 m.addCommand("Draw/slate_v02", "nuke.createNode('slate_v02')")
 m.addCommand("Draw/slate_v03", "nuke.createNode('slate_v03')")
+m.addCommand("Draw/Slate_jonas", "nuke.createNode('Slate_jonas')")
+
+mb = menubar.addMenu("Lazypic")
+mb.addCommand("Issue_and_Bugs", "nukescripts.start('https://github.com/lazypic/nuke/issues')")
+mb.addCommand("Slack", "nukescripts.start('https://lazypic.slack.com')")
+mb.addCommand("Clip Library", "nukescripts.start('https://www.youtube.com/channel/UC0L_YtB4PWSkOwp2m9587MA/playlists?view_as=subscriber')")
+
+mb = menubar.addMenu("kdy")
+mb.addCommand("StartPerformanceTimers", "nuke.startPerformanceTimers()", "F6", shortcutContext=2)
+mb.addCommand("StopPerformanceTimers", "nuke.stopPerformanceTimers()", "F7", shortcutContext=2)
+mb.addCommand("CheckEnv", "reload(checkenv);checkenv.main()")
+mb.addCommand("OpenFile", "reload(openfile);openfile.main()", "F8", shortcutContext=2)
+#mb.addCommand("OpenFile_v01", "openfile_v01.openfile()", "F9", shortcutContext=2)
